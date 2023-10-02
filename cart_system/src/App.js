e2da1b7258b503e1a90c6f7178ea06b633c6a8d8
ashish <ashishguptaitself@gmail.com>
2023-10-02 12:04:06 +0530

    products fetching

--- cart_system/src/App.js
+++ cart_system/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import { Products } from './features/products/Products';
 
 function App() {
   return (
     <div className="App">
-      
+      <Products/>
     </div>
   );
 }
--- cart_system/src/features/cart/cartSlice.js
+++ cart_system/src/features/cart/cartSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchProducts } from './productsAPI';
+import { fetchProducts } from './cartAPI';
 
 const initialState = {
   products: [],
